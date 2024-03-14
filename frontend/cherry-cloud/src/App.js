a9c5e0e94c2b259114c629ad1209272d94358610
AgneDJ <fizikee@gmail.com>
2024-03-13 23:58:59 -0700

    Add logo

--- frontend/cherry-cloud/src/App.js
+++ frontend/cherry-cloud/src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to CHERRY CLOUD!</p>
         <a
           className="App-link"
@@ -15,6 +13,8 @@ function App() {
         >
           Let's code!
         </a>
+
+        <img src="cherry.webp" className="App-logo" alt="logo" />
       </header>
     </div>
   );
