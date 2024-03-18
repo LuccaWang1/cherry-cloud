07d923c28ed7d71a84ebab919fbac48322fea1f9
AgneDJ <fizikee@gmail.com>
2024-03-18 00:45:26 -0700

    Add flappy_cherry_canvas

--- frontend/cherry-cloud/src/App.jsx
+++ frontend/cherry-cloud/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Background from "./flappy_cherry_components/background";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to CHERRY CLOUD!</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -13,6 +15,8 @@ function App() {
         >
           Let's code!
         </a>
+        <h6>Flappy cherry green background</h6>
+        <Background width={800} height={500} />
 
         <img src="cherry.webp" className="App-logo" alt="logo" />
       </header>
