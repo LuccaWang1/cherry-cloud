ecca9d81244c4d1b2512d26cd6680aea4d9d3482
Lucca Wang <luccawang@gmail.com>
2024-03-19 16:32:35 -0600

    Added two classes/tables for instances and db

--- model.py
+++ model.py
@@ -5,6 +5,34 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model): 
+    """A user, a cherry!"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrementing=True, primary_key=True)
+    username = db.Column(db.String(25), nullable=False)
+    user_email = db.Column(db.String(40), unique=True, nullable=False)  
+
+    posts = db.relationship("Post", back_populates="users") #add secondary if many to many relationship - thinking yes 
+
+    def __repr__(self):
+        return f'<User id={self.user_id} ={self.username}>' 
+
+class Post(db.Model):
+    """A review, like a blog post."""
+
+    __tablename__ = "posts"
+
+    post_id = db.Column(db.Integer, autoincrementing=True, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+    users = db.relationship("User", back_populates="posts") #add secondary if many to many relationship - thinking yes 
+
+    def __repr__(self):
+        return f'<Post id={self.post_id} ={self.title}>'
+
 
 def connect_to_db(flask_app, db_uri="postgresql:///cherry-cloud", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
