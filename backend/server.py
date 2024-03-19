133e90245397e500d0339876d00bb420e926697a
Lucca Wang <luccawang@gmail.com>
2024-03-18 19:41:52 -0600

    Adding to gitignore and server.py

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,22 @@
 """Server for cloud-cherry app."""
 
+import os
+from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
 import json
 
 app = Flask(__name__)
-
+app.secret_key = os.environ["KEY"]
+app.jinja_env.undefined = StrictUndefined
 
 @app.route("/")
 def homepage():
     """View homepage."""
+
     return render_template("homepage.html")
 
 
 if __name__ == "__main__":
     connect_to_db(app)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
