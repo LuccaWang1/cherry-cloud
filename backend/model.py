b5fa6844de7060010f3e2a1af536a993d9367409
Lucca Wang <luccawang@gmail.com>
2024-04-11 15:01:28 -0600

    Updated model.py, added associative table for Cherry-Cart relationship

--- backend/model.py
+++ backend/model.py
@@ -5,12 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 #USER STORIES: 
 #A user can have many posts, but a post will only have one user. User-Post is a one-to-many relationship between tables.
 #A user can have many feedback comments, but a feedback comment will only have one user. User-Feedback is a one-to-many relationship between tables.
 #A user can add many cherries in their cart in session, but a cherry only has one user. User-Cherry is a one-to-many relationship between tables. Notes: use conditional rendering to tell the user if what's in their cart is not longer in stock with a flash message - and also on the products page with a background color nothing or white with color font "out of stock", possible: watched by x users.
-
+#A user can have one cart, and one cart has one user. User-Cart is a one-to-one relationship between tables. 
+#A cart can have many cherries, and a cherry can have many carts. Cart-Cherry is a many-to-many relationship between tables - need associative table.
 
 class User(db.Model): 
     """Cherry Cloud user/customer."""
@@ -20,7 +20,8 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String(25), nullable=False)
     user_email = db.Column(db.String(40), unique=True, nullable=False)
-    user_password = db.Column(db.String(10), unique=True, nullable=False) 
+    user_password = db.Column(db.String(10), unique=True, nullable=False)
+    user_personal_best = db.Column(db.Integer) #flappy_cherry score
 
     #posts = db.relationship("Post", back_populates="users") 
 
@@ -82,6 +83,8 @@ class Cherry(db.Model):
     price = db.Column(db.Float, nullable=False) #10, 4, 2
     image_url = db.Column(db.String(200), nullable=False)
 
+    carts = db.relationship("Cart", back_populates="cherries", secondary="cherry_carts")
+
     def __init__(self, cherry_type, price, image_url):
         self.cherry_type = cherry_type 
         self.price = price 
@@ -89,6 +92,36 @@ class Cherry(db.Model):
 
     def __repr__(self):
         return f"<Cherry: {self.cherry_type}, {self.price}>"
+    
+class Cherry_Cart(db.Model):
+    """A cherry cart associative table for the many-to-many relationship between Cherry and Cart."""
+
+    __tablename__ = "cherry_carts"
+
+    cherry_cart_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    cherry_id = db.Column(db.Integer, db.ForeignKey("cherries.cherry_id"))
+    cart_id = db.Column(db.Integer, db.ForeignKey("carts.cart_id"))
+
+    def __repr__(self):
+        return f"<Cherry_cart_id: Cherry_id={self.cherry_id} cart_id={self.cart_id}>"
+
+    
+class Cart(db.Model):
+    """A Cart instance."""
+
+    __tablename__ = "carts"
+
+    cart_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+    cherries = db.relationship("Cherry", back_populates="carts", secondary="cherry_carts")
+
+    def __init__(self, cherry_type, price, image_url):
+        self.cherry_type = cherry_type 
+        self.price = price 
+        self.image_url = image_url 
+
+    def __repr__(self):
+        return f"<Cherry: {self.cherry_type}, {self.price}>"   
 
 def connect_to_db(flask_app, db_uri="postgresql:///cherry-cloud", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
