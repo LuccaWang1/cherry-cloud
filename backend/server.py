fa495a7bc3d19b74afe5983551cd689dbec19596
AgneDJ <fizikee@gmail.com>
2024-04-17 14:17:58 -0700

    Changes paths

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,9 @@ from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static',
+            static_url_path='/static')
+
 # app.secret_key = os.environ["KEY"]
 app.jinja_env.undefined = StrictUndefined
 
